ceed9ad24eca408fa81e2f6b9fb95e88fb90aac6
yunko566 <yunko566@naver.com>
2020-09-15 22:55:55 +0900

    Edit readme.md

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 ## Movie_App_2020
 
 React Js Fundamentals Course 2020
+
+## 09월11일
+>1. Dothome에 indexfile
+수정하기
+>2. readme.md 수정 후 Github에 push하기.
+* 언제까지? 9월16일까지.
--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 
 function App() {
-  return <div className="App" /> ;
+  // return <div className="App" /> ;
+   return(
+   <div>
+     <h1>Hello</h1>
+     </div>
+   );
 }
 
 export default App;
