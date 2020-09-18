36035f56344c1b68a0dbc3813f16c7dc50f6b2c9
yunko566 <yunko566@naver.com>
2020-09-18 12:11:53 +0900

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+function Food({ fav }) {
+
+  return (
+    <h3>I Love {fav}</h3>
+  )
+}
 
 function App() {
-  // return <div className="App" /> ;
+  
    return(
    <div>
      <h1>Hello</h1>
+     <Food fav="김치"  />
+     <Food fav="짜장면"  />
+     <Food fav="짬뽕"  />
+     <Food fav="만두"  />
+     <Food fav="치킨"  />
      </div>
    );
 }
