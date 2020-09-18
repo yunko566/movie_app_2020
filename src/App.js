890e5a75791b48b1c9b88e805869cae097b9cf5a
yunko566 <yunko566@naver.com>
2020-09-18 12:46:21 +0900

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 
 function Food({ fav }) {
-
   return (
     <h3>I Love {fav}</h3>
   )
 }
+
+const foodLike = [
+  {
+    name: '김치',
+  },
+  {
+    name: '치킨',
+  },
+  {
+    name: '삼겹살',
+  },
+];
 
 function App() {
   
    return(
    <div>
      <h1>Hello</h1>
-     <Food fav="김치"  />
-     <Food fav="짜장면"  />
-     <Food fav="짬뽕"  />
-     <Food fav="만두"  />
-     <Food fav="치킨"  />
      </div>
    );
 }
