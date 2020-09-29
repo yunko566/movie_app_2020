73b2640993088e8fae904d6d44709824363474e7
yunko566 <yunko566@naver.com>
2020-09-29 21:03:51 +0900

    5주차 수업 내용 커밋

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import React from 'react';
+import img01 from './image/1.jpg';
+import img02 from './image/2.jpg';
+import img03 from './image/3.jpg';
+import img04 from './image/4.jpg';
+import img05 from './image/5.jpg';
 
-function Food({ fav }) {
+
+function Food({name, picture, message0}) {
   return (
-    <h3>I Love {fav}</h3>
-  )
+    <div>
+      <h2>I Love {name}</h2>
+      <img src={picture} alt={message0}/>
+    </div>
+  );
 }
 
-const foodLike = [
+const foodILike = [
   {
-    name: '김치',
+    id: 1,
+    name: '햄버거',
+    image: img01,
+    message0: '햄버거 사진'
   },
   {
-    name: '치킨',
+    id: 2,
+    name: '연어',
+    image: img02,
+    message0: '연어 사진'
   },
   {
-    name: '삼겹살',
+    id: 3,
+    name: '닭발',
+    image: img03,
+    message0: '닭발 사진'
+  },
+  {
+    id: 4,
+    name: '호떡',
+    image: img04,
+    message0: '호떡 사진'
+  },
+  {
+    id: 5,
+    name: '스테이크',
+    image: img05,
+    message0: '스테이크 사진'
   },
 ];
 
@@ -22,7 +52,7 @@ function App() {
   
    return(
    <div>
-     <h1>Hello</h1>
+     {foodILike.map(dish =>(<Food key={dish.id} name={dish.name} picture={dish.image} alt0={dish.message0}/>))}
      </div>
    );
 }
