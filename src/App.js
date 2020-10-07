413ec815226c319a4ecaab93aeea051d1d4d830d
yunko566 <yunko566@naver.com>
2020-10-07 12:52:47 +0900

    6주차 실습 및 리드미 커밋

--- src/App.js
+++ src/App.js
@@ -1,60 +1,29 @@
 import React from 'react';
-import img01 from './image/1.jpg';
-import img02 from './image/2.jpg';
-import img03 from './image/3.jpg';
-import img04 from './image/4.jpg';
-import img05 from './image/5.jpg';
 
-
-function Food({name, picture, message0}) {
-  return (
-    <div>
-      <h2>I Love {name}</h2>
-      <img src={picture} alt={message0}/>
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: '햄버거',
-    image: img01,
-    message0: '햄버거 사진'
-  },
-  {
-    id: 2,
-    name: '연어',
-    image: img02,
-    message0: '연어 사진'
-  },
-  {
-    id: 3,
-    name: '닭발',
-    image: img03,
-    message0: '닭발 사진'
-  },
-  {
-    id: 4,
-    name: '호떡',
-    image: img04,
-    message0: '호떡 사진'
-  },
-  {
-    id: 5,
-    name: '스테이크',
-    image: img05,
-    message0: '스테이크 사진'
-  },
-];
-
-function App() {
-  
-   return(
-   <div>
-     {foodILike.map(dish =>(<Food key={dish.id} name={dish.name} picture={dish.image} alt0={dish.message0}/>))}
-     </div>
-   );
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+  add = () => {
+    this.setState( current => ({
+      count: current.count + 1 
+    }) );
+  };
+  Minus = () => {
+    this.setState( current => ({
+      count: current.count - 1 
+  }) );
+};
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>&nbsp;&nbsp;&nbsp;
+        <button onClick={this.Minus}>Minus</button>
+      </div>
+    
+    );
+  }
 }
 
 export default App;
