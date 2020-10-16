08cb3e05c9c559def04e895990ed88e33153d4ec
yunko566 <yunko566@naver.com>
2020-10-16 12:36:48 +0900

    7주차 수업 실습 및 리드미 커밋

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
-    count: 0,
-  }
-  add = () => {
-    this.setState( current => ({
-      count: current.count + 1 
-    }) );
+    isLoding: true,
+    movies: [],
   };
-  Minus = () => {
-    this.setState( current => ({
-      count: current.count - 1 
-  }) );
-};
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+  componentDidMount() {
+    // 영화 데이터 로딩
+    this.getMovies();
+  }
   render() {
+    const { isLoding } = this.state;
     return (
       <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>&nbsp;&nbsp;&nbsp;
-        <button onClick={this.Minus}>Minus</button>
+        {isLoding ? 'Loding...' : 'We are ready'}
       </div>
-    
-    );
+    );    
   }
 }
 
