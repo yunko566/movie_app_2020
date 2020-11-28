6e9c45c465c1b9528dafe8533737f0e301c319f1
yunko566 <yunko566@naver.com>
2020-11-28 11:11:24 +0900

    Updates

---
+++ precache-manifest.ab5e7014eddb1823600af770dae945c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19712da54f24c04ad7257448243da823",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0674c0b30fd1f6021381",
+    "url": "/movie_app_2020/static/css/main.743033b8.chunk.css"
+  },
+  {
+    "revision": "e9d567958cdf7fa01b8b",
+    "url": "/movie_app_2020/static/js/2.3ce463e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.3ce463e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0674c0b30fd1f6021381",
+    "url": "/movie_app_2020/static/js/main.cd6a18b8.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
